e2460f35137c7ae95a51c5af62557c91446a322e
martanilsson <82098567+martanilsson@users.noreply.github.com>
2022-02-18 11:15:22 +0100

    testade bara lite

--- graph.js
+++ graph.js
@@ -134,7 +134,7 @@ function display_list(list, genre) {
     .attr("r", 6)
     .style("fill", color)
 
-    //Hover functuion
+    //Hover function
     .on('mouseover', function (d) {
         d3.select(this)
             .attr('opacity', '.5')
@@ -201,12 +201,6 @@ svg.append("text")
     .attr("x", 30)
     .attr("y", -15)
     .text("percentage");
-
-    $('.Button').click(function(e) {
-        $('.Button').not(this).removeClass('active');    
-        $(this).toggleClass('active');
-        e.preventDefault();
-    });
 
 
 
